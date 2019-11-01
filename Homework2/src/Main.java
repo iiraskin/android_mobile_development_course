9790020982ee5531e3b08e886ed9bc096e343e32
Ilya Raskin <iraskin@mail.ru>
2019-11-01 23:29:44 +0300

    Second homework

---
+++ Homework2/src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+
+    private static void printArray(int[][] arr)
+    {
+        for (int[] str : arr) {
+            for (int el : str) {
+                System.out.print(el);
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+        System.out.println(""); //Чтобы отделить выводимые массивы друг от друга
+    }
+
+    /* Выносит максимальный элемент массива на первое место */
+    private static void biggestToFirst(int[] arr)
+    {
+        for (int i = 1; i < arr.length; ++i) {
+            if (arr[i] > arr[0]) {
+                int tmp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][7];
+        for (int i = 0; i < arr.length; ++i) {
+            for (int j = 0; j < arr[i].length; ++j) {
+                arr[i][j] = ((int) (Math.random() * 10));
+            }
+        }
+
+        printArray(arr);
+
+        for (int[] str : arr) {
+            biggestToFirst(str);
+        }
+
+        printArray(arr);
+
+        //Пример работы с созданными классами.
+        Point a = new Point(-3, 4);
+        Point b = new Point();
+        Rect r = new Rect(a, b);
+
+        System.out.println(RectUtils.square(r));
+        System.out.println(RectUtils.perimeter(r));
+        System.out.println(RectUtils.diagonal(r));
+    }
+}
---
+++ Homework2/src/Point.java
@@ -0,0 +1,19 @@
+public final class Point {
+    private int x;
+    private int y;
+
+    public Point(){}
+
+    public Point(int x_, int y_) {
+        x = x_;
+        y = y_;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
